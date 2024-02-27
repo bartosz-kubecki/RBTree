084332bb154ad70523d17277cefc7663bb3468d0
Bartosz Kubecki <Bartosz.Kubecki@tm1.edu.pl>
2024-02-27 21:33:24 +0100

    Create RBTree.java

---
+++ src/RBTree.java
@@ -0,0 +1,11 @@
+public class RBTree {
+    private Node root;
+
+    public void add(int key, int value);
+
+    public int remove(int key);
+
+    public int get(int key);
+
+    public int height();
+}

a9610c6c0e993df4f55153339f4d6c6747208071
Bartosz Kubecki <Bartosz.Kubecki@tm1.edu.pl>
2024-02-28 13:32:54 +0100

    Rotation methods

--- src/RBTree.java
+++ src/RBTree.java
@@ -1,6 +1,52 @@
 public class RBTree {
     private Node root;
 
+    private void rotateRight(Node node) {
+        Node parent = node.parent;
+        Node leftChild = node.left;
+
+        node.left = leftChild.right;
+        if (leftChild.right != null) {
+            leftChild.right.parent = node;
+        }
+
+        leftChild.right = node;
+        node.parent = leftChild;
+
+        if (parent == null) {
+            root = leftChild;
+        } else if (parent.left == node) {
+            parent.left = leftChild;
+        } else if (parent.right == node) {
+            parent.right = leftChild;
+        }
+
+        leftChild.parent = parent;
+    }
+
+    private void rotateLeft(Node node) {
+        Node parent = node.parent;
+        Node rightChild = node.right;
+
+        node.right = rightChild.left;
+        if (rightChild.left != null) {
+            rightChild.left.parent = node;
+        }
+
+        rightChild.left = node;
+        node.parent = rightChild;
+
+        if (parent == null) {
+            root = rightChild;
+        } else if (parent.left == node) {
+            parent.left = rightChild;
+        } else if (parent.right == node) {
+            parent.right = rightChild;
+        }
+
+        rightChild.parent = parent;
+    }
+
     public void add(int key, int value);
 
     public int remove(int key);

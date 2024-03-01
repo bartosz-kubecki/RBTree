cd4105cb48a688edc89b51dc47178e2a6fa2747e
Bartosz Kubecki <Bartosz.Kubecki@tm1.edu.pl>
2024-03-02 00:14:19 +0100

    Add generating random tree and printing it

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        RBTree t = new RBTree();
+        Random r = new Random();
+
+        for (int i = 0; i < 20; i++) {
+            try {
+                t.add(r.nextInt(1, 40));
+            } catch (IllegalArgumentException e) {
+                i--;
+            }
+        }
+
+        System.out.println(t);
+        System.out.print(t.height());
     }
 }

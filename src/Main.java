cbb12179d90bd56707fdc72ba6334e7fd5361af1
Bartosz Kubecki <Bartosz.Kubecki@tm1.edu.pl>
2024-03-19 18:32:45 +0100

    Add removing nodes

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,33 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        RBTree t = new RBTree();
+        RBTree tree = new RBTree();
         Random r = new Random();
 
         for (int i = 0; i < 20; i++) {
             try {
-                t.add(r.nextInt(1, 40));
+                int n = r.nextInt(1, 40);
+                tree.add(n);
+                System.out.println("Adding " + n);
+                System.out.println(tree);
             } catch (IllegalArgumentException e) {
                 i--;
             }
         }
 
-        System.out.println(t);
-        System.out.print(t.height());
+        System.out.println("Height: " + tree.height());
+
+        for (int i = 0; i < 20; i++) {
+            try {
+                int n = r.nextInt(1, 40);
+                tree.remove(n);
+                System.out.println("Removing " + n);
+                System.out.println(tree);
+            } catch (IllegalArgumentException e) {
+                i--;
+            }
+        }
+
+        System.out.println(tree);
     }
 }
